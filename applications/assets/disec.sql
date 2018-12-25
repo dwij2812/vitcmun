f46651ed290b60a1566c9e501f8634f474b2af75
dwij2812 <dwijhariket@gmail.com>
2018-12-26 02:45:00 +0530

    Added DB structure for DISEC Candidates

---
+++ applications/assets/disec.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 24, 2018 at 10:52 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vitcmun`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `disec`
+--
+
+CREATE TABLE `disec` (
+  `country` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `disec`
+--
+
+INSERT INTO `disec` (`country`) VALUES
+('(Islamic Republic of) Afghanistan'),
+('Albania'),
+('Algeria'),
+('Angola'),
+('Argentine Republic'),
+('Armenia'),
+('Australia'),
+('Austria'),
+('Bahrain( Kingdom of)'),
+('Azerbaijan'),
+('Bahamas (Commonwealth of the)'),
+('Bangladesh(People''s Republic of)'),
+('Barbados'),
+('Belarus'),
+('Belgium'),
+('Bhutan (Kingdom of)'),
+('Bolivia (Plurinational State Of)'),
+('Brazil'),
+('Bulgaria'),
+('Combodia'),
+('Cameroon'),
+('Canada'),
+('Central African Republic'),
+('Chad'),
+('Chile (Republic of)'),
+('Colombia'),
+('Comoros'),
+('Republic of Congo'),
+('Costa Rica'),
+('Croatia'),
+('Cuba(Republic of)'),
+('Cyprus'),
+('Czech Republic'),
+('Democratic Republic of Congo'),
+('Democratic People''s Republic of Korea'),
+('Denmark'),
+('Djibouti'),
+('Dominican Republic'),
+('Ecuador'),
+('Egypt (Arab Republic of)'),
+('El Salvador'),
+('Estonia'),
+('Ethiopia'),
+('Fiji'),
+('Finland'),
+('Former Yugoslavia Republic of Macedonia'),
+('France'),
+('Georgia'),
+('Germany'),
+('Ghana'),
+('Greece'),
+('Guatamela'),
+('Guinea'),
+('Haiti'),
+('Honduras'),
+('Hungary'),
+('Iceland'),
+('India'),
+('Indonesia'),
+('Islamic Republic of Iran'),
+('Republic of Iraq'),
+('Ireland'),
+('Israel'),
+('Italy'),
+('Jamaica'),
+('Japan'),
+('Jordan'),
+('Kazakhstan'),
+('Kenya'),
+('Kiribati'),
+('Kuwait'),
+('Kyrgyzstan'),
+('Latvia'),
+('Lebanon'),
+('Liberia'),
+('Lithuania'),
+('Luxembourg'),
+('Madagascar'),
+('Malaysia'),
+('Maldives'),
+('Malta'),
+('Mauritius'),
+('Mexico'),
+('Monaco'),
+('Mongolia'),
+('Morocco'),
+('Myanmar'),
+('Namibia'),
+('Nepal'),
+('Netherlands'),
+('New Zealand'),
+('Nicaragua'),
+('Nigeria'),
+('Norway'),
+('Oman'),
+('Pakistan'),
+('Panama'),
+('Papua New-Guinea'),
+('Paraguay'),
+('People''s Republic of China'),
+('Philippines'),
+('Poland'),
+('Portugal'),
+('Qatar'),
+('Republic of Korea'),
+('Romania'),
+('Russian Federation'),
+('Saudi Arabia'),
+('Serbia'),
+('Sierra Leone'),
+('Singapore'),
+('Slovakia'),
+('Somalia'),
+('South Africa'),
+('South Sudan'),
+('Spain'),
+('Sri Lanka'),
+('Sudan'),
+('Sweden'),
+('Switzerland'),
+('Syrian Arab Republic'),
+('Tajikistan'),
+('Thailand'),
+('Trinidad and Tobago'),
+('Tunisia'),
+('Turkey'),
+('Uganda'),
+('Ukraine'),
+('United Arab Emirates'),
+('United Kingdom of Great Britain'),
+('United Republic of Tanzania'),
+('United States of America'),
+('Uruguay'),
+('Uzbekistan'),
+('Bolivarian Republic of Venezuela'),
+('Vietnam'),
+('Yemen'),
+('Zambia'),
+('Zimbabwe'),
+('Palestine ( Observer )'),
+('Libya (Observer)');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
