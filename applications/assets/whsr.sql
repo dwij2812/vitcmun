934cb44ccb75f893f13c124c86ea4941b36af132
dwij2812 <dwijhariket@gmail.com>
2019-01-14 03:45:00 +0530

    Added SQL Structure file for WHSR

--- applications/assets/whsr.sql
+++ applications/assets/whsr.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 24, 2018 at 10:56 AM
+-- Generation Time: Jan 11, 2019 at 05:17 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -29,6 +29,28 @@ SET time_zone = "+00:00";
 CREATE TABLE `whsr` (
   `country` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `whsr`
+--
+
+INSERT INTO `whsr` (`country`) VALUES
+('Tulsi Gabbard, Secretary of State'),
+('Robert Gates, Secretary of Defence'),
+('Janet Napolitano, Secretary of Homeland Security'),
+('Michael Hayden, Director- CIA'),
+('James Comey, Director- FBI'),
+('Jeff Prescott, National Security Advisor to the President'),
+('Charles Frank Bolden, NASA Administrator'),
+('GEN Mark A. Milley, Chief of Staff- Army'),
+('ADM John M. Richardson, Chief of Naval Operations'),
+('Gen David L. Goldfein, Chief of Staff- Air Force'),
+('Joseph Francis Dunford Jr., Chairman- Joint Chiefs of Staff'),
+('Gen Joseph Votel, Commander USCENTCOM'),
+('GEN Paul M. Nakasone, Commander US Cyber COM'),
+('Eric Shinseki, Director, National Security Agency'),
+('LTG Richard D. Clarke, Commander, US Special Operations Command'),
+('Brett McGurk, US Special Envoy in Syria');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
