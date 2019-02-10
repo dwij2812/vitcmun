96afa56e95eca02fe62e5a5826db83b6615938e5
dwij2812 <dwijhariket@gmail.com>
2019-02-11 00:43:00 +0530

    Added Buttons and Photography apps

--- photography/Thank_you.php
+++ photography/Thank_you.php
@@ -1,3 +1,44 @@
-<?php
-echo "Kindly stay tuned to the page where we will release the photographer allotments as a part of  Round-3 allotments via Know Your Allotment.";
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>VITCMUN: Photography Applications</title>
+	<link rel="stylesheet" href="assets/demo.css">
+	<link rel="stylesheet" href="assets/form-labels-on-top.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+	<meta name="theme-color" content="#000000" />
+
+</head>
+<body bgcolor="#1c1c1c">
+
+	<header>
+		<h1>VITCMUN: Photography Applications</h1>
+    </header>
+	<br><Br>
+    <div class="main-content animated fadeInRight">
+	<br><br><br>
+        <form class="form-labels-on-top" method="post" action="success.html">
+
+            <div class="form-title-row">
+                <h1>Thank You!</h1><br>
+            </div>
+			<div class="form-row">
+                <label>
+				<center>
+                    <h4>Kindly stay tuned to the page where we will release the photographer allotments as a part of  Round-3 allotments via Know Your Allotment.</h4>
+                </label>
+				</center>
+            </div>
+            <br><br><br>
+        </form>
+    </div>
+	<br><br>
+	<footer>
+		<h1>Debate.     Delegate.     Decimate.</h1>
+    </footer>
+</body>
+</html>
